4e896846a7e50e3e758c4a2a775bd38833073f43
yshiraishiy <yuya0505soccer@gmail.com>
2022-03-09 21:47:32 +0900

    commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  alert(inputText);
+};
+
+document.getElementById("add-button").addEventListener("click", () => {
+  onClickAdd()
+});
