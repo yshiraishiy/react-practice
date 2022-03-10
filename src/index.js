5ae605d56aa3b0f4816f47ebe4495d148f38dcde
yshiraishiy <yuya0505soccer@gmail.com>
2022-03-10 13:47:05 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -3,9 +3,66 @@
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  alert(inputText);
+
+  createIncompleteList(inputText);
 };
 
+const deleteFromIncompleteList = (target) => {
+  // 4 const deleteTarget = deleteButton.parentNode;
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+const createIncompleteList = (text) => {
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  // 6 
+  li.innerText = text;
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    deleteFromIncompleteList(completeButton.parentNode);
+    // 5 関数化したので削除
+    // const deleteTarget = completeButton.parentNode;
+    // document.getElementById("incomplete-list").removeChild(deleteTarget);
+    const addTarget = completeButton.parentNode;
+    const text = addTarget.firstElementChild.innerText;
+    addTarget.textContent = null;
+    const li = document.createElement("li");
+    li.innerText = text;
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget)
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    })
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+    document.getElementById("complete-list").appendChild(addTarget);
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    deleteFromIncompleteList(deleteButton.parentNode);
+    // 5 関数化したので削除 
+    // const deleteTarget = deleteButton.parentNode;
+    // document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+//2
 document.getElementById("add-button").addEventListener("click", () => {
-  onClickAdd()
+  onClickAdd();
 });
